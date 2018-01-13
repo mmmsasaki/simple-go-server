4c0ea6703498b684c343ee65fe8e88af5af2c295
mmmsasaki <sasaki@mmmcorp.co.jp>
2018-01-13 15:05:15 +0900

    [add] test settings

---
+++ test/hooks.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/snikch/goodman/hooks"
+	trans "github.com/snikch/goodman/transaction"
+)
+
+func main() {
+	h := hooks.NewHooks()
+	server := hooks.NewServer(hooks.NewHooksRunner(h))
+	h.Before("/message > GET", func(t *trans.Transaction) {
+		fmt.Println("before modification")
+	})
+	server.Serve()
+	defer server.Listener.Close()
+}
